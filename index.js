cd0cd5d9b412e33f234dee9471331cefa2330733
Jesse Brown <Brownjesse8895@yahoo.com>
2021-06-22 12:46:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);  
+
+let btn = document.getElementById('text')
+
+btn.addEventListener('click', (e) =>){
+    console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered")
+}
