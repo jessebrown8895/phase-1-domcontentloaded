a0c05c97cab678e6b003355d5ce1f33a90d5ecf7
Jesse Brown <Brownjesse8895@yahoo.com>
2021-10-18 20:45:50 -0500

    commit tree d659fe2a3cfc8e9ed8f0241a7c407697771056d8

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-let vari = document.getElementById('text')
-vari.innerHTML = "This is really cool!";
-});
+// document.addEventListener("DOMContentLoaded", function() {
+// let vari = document.getElementById('text')
+// vari.innerHTML = "This is really cool!";
+// });
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const text = document.getElementById("text")
+    text.innerHTML = "This is really cool!"
+})
