ad12a77f45e0bcf753bc15d1cbbeba649f3c58a4
Jesse Brown <Brownjesse8895@yahoo.com>
2021-10-18 20:47:22 -0500

    DOMContentLoaded lab
    created an event listener the event the document is listening for is DOMContentloaded the event llistener changes the p tag with an id of text to This is really cool

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-let vari = document.getElementById('text')
-vari.innerHTML = "This is really cool!";
-});
+document.addEventListener("DOMContentLoaded", () =>{
+    const text = document.getElementById("text")
+    text.innerHTML = "This is really cool!"
+})
